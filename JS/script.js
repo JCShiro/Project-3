681902541ce5a1e872d7ecf494f3f013c40870af
JCShiro <jacob.shadbolt@student.norwichuni.ac.uk>
2025-11-13 21:52:01 +0000

    added comments

--- JS/script.js
+++ JS/script.js
@@ -39,17 +39,25 @@ const planetImg = [
   },
   {
     biomeName: "undergrowth",
-    image: "Images/Biomes/Jungle.png"
-  }
+    image: "Images/Biomes/Jungle.png",
+  },
 ];
 //References
-let dataIndex = -1;
+//Create an index value for data elements
+let dataIndex = 0;
+//Get a JS reference for the selectors
 const locations = document.getElementById("planets");
+//get a reference for the faction image
 const CurrentFactionImg = document.getElementById("planet_img");
+//get a reference for the faction text
 const factionText = document.getElementById("faction_text");
+// get a reference for the liberation percentage text
 const libText = document.getElementById("lib_text");
+// get a reference for the divers active text
 const activeText = document.getElementById("active_text");
+// get a reference for the terrain type text
 const terrainText = document.getElementById("terrain_text");
+// get a reference for the terrain image
 const planetImages = document.getElementById("biome_img");
 //helldivers 2 planet api
 const apiUrl = "https://helldiverstrainingmanual.com/api/v1/war/campaign";
@@ -57,106 +65,96 @@ const apiUrl = "https://helldiverstrainingmanual.com/api/v1/war/campaign";
 // LOCAL DATA
 // const apiUrl = "http://127.0.0.1:5500/offlineData.json"
 //FUNCTIONS
+locations.onchange = () => {
+  //   console.log("onchange") //Testing log
+  const ID = locations.value;
+  fetchData(ID);
+};
+//<----------fetchData FUNCTION---------->
 async function fetchData(ID) {
+  //async means function does not need to be called. The ID parameter is passed through the function
   try {
     // console.log("fetch") //Testing log
-    // const planetURL = apiUrl //+ "/" + ID;
     // console.log(planetURL)
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl); // Checks for a response from the api URL
     if (!response.ok) {
-      throw new Error("response status", response.status);
+      //checks to see if there was a good response
+      throw new Error("response status", response.status); //creates a new error and prints it to console
     }
-    const json = await response.json();
-    // console.log(json);
+    const json = await response.json(); //sets json to be the api's json file
+    console.log(json); //Console for testing and dev
     //we now have data
+    //calls the function processData and passes through json and ID
     processData(json, ID);
     // populateUI(json);
   } catch (err) {
     console.error(err);
   }
 }
-function populateUI(data) {
-  document.getElementById("planet_text").innerHTML =
-    "You are viewing: " + data.name;
-  factionText.innerHTML = "Current owner: " + data.faction;
-  libText.innerHTML = "Liberated: " + Math.floor(data.percentage) + "%";
-  activeText.innerHTML = "Divers active: " + data.players;
-  try{
-    terrainText.innerHTML = "Terrain: " + data.biome.slug;
-  }catch{
-    terrainText.innerHTML = "Planet Inactive"
-  }
-  //   console.log(data.biome.slug);
-  factionSelector(data);
-  //   console.log(data.faction);
-  //   console.log(data.planetIndex); //Console for testing
-  BiomeImages(data, planetImg);
-}
-locations.onchange = () => {
-  //   console.log("onchange") //Testing log
-  const ID = locations.value;
-  fetchData(ID);
-};
+//<----------processData FUNCTION---------->
 function processData(json, ID) {
+  //creates an itterative loop that starts at 0 and ends at the length of the array in the json file
   for (let i = 0; i < json.length; i++) {
     // console.log(json[i].biome.slug); //Console for development
     if (json[i].planetIndex == ID) {
-      dataIndex = i;
-      console.log("Found Planet");
+      //checks to see if the iterated planet id matches the selected planet id, if not the for loop is continued
+      dataIndex = i; //sets the dataIndex to euqal the iterated value
+      console.log("Found Planet"); //Console log for testing and debugging
 
-      populateUI(json[dataIndex]);
+      populateUI(json[dataIndex]); //calls the populateUI function and passes the selected json objects
       break;
     }
   }
   if (dataIndex == -1) {
+    //if the planet isnt found, call a function that sets up a default response
     console.log("Planet not Found");
     defaultUi();
   }
 }
+//<----------populateUI FUNCTION---------->
+function populateUI(data) {
+  //named function with the data parameter passed through
+  document.getElementById("planet_text").innerHTML =
+    "You are viewing: " + data.name; //gets a reference for the planet text and changes the dext displayed to show the planets name
+  factionText.innerHTML = "Current owner: " + data.faction; //changes the fation text to display the occupying faction
+  libText.innerHTML = "Liberated: " + Math.floor(data.percentage) + "%"; //changes the liberated text to show the %liberated
+  activeText.innerHTML = "Divers active: " + data.players; //changes the text to show how many players are active
+  try {
+    //tries to get the terrain data
+    terrainText.innerHTML = "Terrain: " + data.biome.slug; //if successfull, changes the text
+  } catch {
+    terrainText.innerHTML = "Planet Inactive"; //if fails, change text to this
+  }
+  //   console.log(data.biome.slug);
+  factionSelector(data); //calls factionSelector and passes data through it
+  //   console.log(data.faction);
+  //   console.log(data.planetIndex); //Console for testing
+  BiomeImages(data, planetImg); //calls BiomeImages and passes data and planetImg array
+}
+//<----------factionSelector FUNCTION---------->
 function factionSelector(data) {
   if (data.faction == "Illuminates") {
-    CurrentFactionImg.src = "Images/Factions/Illuminate_Icon.webp";
+    //checks what the faction is
+    CurrentFactionImg.src = "Images/Factions/Illuminate_Icon.webp"; //changes the image if true
   } else if (data.faction == "Automatons") {
+    //repeat with new faction name
     CurrentFactionImg.src = "Images/Factions/Automaton_icon.webp";
   } else if (data.faction == "Terminids") {
+    //repeat with new faction name
     CurrentFactionImg.src = "Images/Factions/Terminids_Icon.webp";
   } else {
-    CurrentFactionImg.src = "Images/Factions/Super_Earth_Icon.webp";
+    CurrentFactionImg.src = "Images/Factions/Super_Earth_Icon.webp"; //if not one of the 3 stated factions, default to this image
   }
 }
+//<----------BiomeImages FUNCTION---------->
 function BiomeImages(data) {
-  console.log(data.biome.slug);
-  // console.log(planetImages)
-  // if (data.biome.slug == "moon") {
-  //   planetImages.src = "Images/Biomes/Moon.png";
-  //   // console.log(planetImages)
-  // } else if (data.biome.slug == "crimsonmoor") {
-  //   planetImages.src = "Images/Biomes/CrimsonMoor.png";
-  // } else if (data.biome.slug == "icemoss") {
-  //   planetImages.src = "Images/Biomes/IceMoss.png";
-  // } else if (data.biome.slug == "jungle") {
-  //   planetImages.src = "Images/Biomes/Jungle.png";
-  // } else if (data.biome.slug == "canyon") {
-  //   planetImages.src = "Images/Biomes/Sandy.png";
-  // } else if (data.biome.slug == "swamp") {
-  //   planetImages.src = "Images/Biomes/Swamp.png";
-  // } else if (data.biome.slug == "winter") {
-  //   planetImages.src = "Images/Biomes/Winter.png";
-  // } else if (data.biome.slug == "mesa" || "desert") {
-  //   planetImages.src = "Images/Biomes/Mesa.png";
-  // } else if (data.biome.slug == "toxic") {
-  //   planetImages.src = "Images/Biomes/Toxic.png";
-  // } else if (data.biome.slug == "desolate") {
-  //   planetImages.src = "Images/Biomes/Sandy.png";
-  // } else {
-  //   planetImages.src = "Images/Biomes/TextSuperDestroyerLibertyRefined.png";
-  //   console.log("error");
-  // }
-  // console.log(planetImages.src);
-  for(i=0;i<planetImg.length;i++){
-    if(data.biome.slug == planetImg[i].biomeName){
-      planetImages.src = planetImg[i].image
-      break
+  // console.log(data.biome.slug); //Console log for development
+  for (i = 0; i < planetImg.length; i++) {
+    //for loop that itterates through the array
+    if (data.biome.slug == planetImg[i].biomeName) {
+      //if the terrain data matches the selected terrain name in the array
+      planetImages.src = planetImg[i].image; //change the image to matching image
+      break;
     }
   }
 }
